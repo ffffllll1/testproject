c2ca47dc024f7417a3890d6c87942e091953dd40
user <realgnome@ukr.net>
2024-08-16 19:13:32 +0300

    h/m

--- homework.py
+++ homework.py
@@ -1,11 +1,33 @@
 
-numbers = [1, -2, 3, -4, 0, 6, 337, -108, 49, 0]
-for i in numbers:                                      #1
-    if i % 2 == 0:
-        print(i, end = '  ' )
+numbers=[3,1,2,4,5]
+
+my_list = []
+my_list2 = []
+
+for elem in numbers:
+    if elem % 2 == 0:                                      #1
+        my_list.append(elem)
+    else:
+        my_list2.append(elem)
+main_list = my_list + my_list2
+print(main_list)
 
 
 
-for i in numbers:
-    if i > numbers[8]:                                 #2
-        print('\n максимальне число:', i )
+numbers=[1,4,1,5,56,-5,7,4,78,-1,-5]
+i = 0
+for elem in numbers:                                        #2
+    i += elem
+print('сума всіх чисел\t', i)
+
+numbers=[0,5,15,6,-5,9,-1,5,5]
+number = []
+number2 = []
+for elem in numbers:
+    if elem < 0:
+        number = elem                                       #3
+print('останній відємний елемент\t', number)
+for elem in numbers:
+    if elem > 0:
+        number2 = elem
+print('перший додатній елемент\t', number2)
